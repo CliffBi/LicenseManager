4ac3adb6fa9df9a87620e77fdccee40643d3416a
CliffBi <dimashestak5@gmail.com>
2022-01-27 17:06:44 +0200

    add MAIN Licenses and db

--- MAIN/Licenses/models.py
+++ MAIN/Licenses/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Licenses(models.Model):
-    license_name = models.CharField(max_length=30)
+    license_name = models.CharField(max_length=30, help_text='Enter License name')
     copyright_holder = models.CharField(max_length=30)
     product_name = models.CharField(max_length=30)
     guarantees = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['license_name']
+
+    def __str__(self):
+        return self.field_names
+
 
