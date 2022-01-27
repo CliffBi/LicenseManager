79d28c752e33910557e6994375e74c14ba6d7acc
CliffBi <dimashestak5@gmail.com>
2022-01-27 16:09:28 +0200

    add MAIN Licenses and db

--- MAIN/MAIN/settings.py
+++ MAIN/MAIN/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'Licenses',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'MAIN.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db1.sqlite3',
     }
 }
 
