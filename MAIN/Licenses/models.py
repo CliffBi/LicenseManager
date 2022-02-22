e463642610308f4864d145283bbf52ed0745f978
CliffBi <dimashestak5@gmail.com>
2022-02-22 16:43:00 +0200

    update: models.Licenses, views.py add: db_tables.json

--- MAIN/Licenses/models.py
+++ MAIN/Licenses/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class Licenses(models.Model):
     license_name =      models.CharField(max_length=30)
     # copyright_holder = models.CharField(max_length=100)
-    guarantee =         models.BooleanField(help_text='Is the license guaranteed?', blank=True, null=True)
-    DFSG_compatible =   models.BooleanField(help_text='')
-    FSF_approved =      models.BooleanField(help_text='')
-    OSI_approved =      models.BooleanField(help_text='Is the OSI license approved?')
-    GPL_compatible =    models.BooleanField(help_text='Is it compatible with GPL?')
-    copyleft =          models.BooleanField(help_text='Is Copyleft a license?', default=True)
-    different_license = models.BooleanField(help_text='Allows linking with code under a different license.')
-    active =            models.BooleanField(help_text='Check', default=True)
+    guarantee =         models.BooleanField(blank=True, null=True, help_text='Is the license guaranteed?')
+    DFSG_compatible =   models.BooleanField(blank=True, null=True, help_text='')
+    FSF_approved =      models.BooleanField(blank=True, null=True, help_text='')
+    OSI_approved =      models.BooleanField(blank=True, null=True, help_text='Is the OSI license approved?')
+    GPL_compatible =    models.BooleanField(blank=True, null=True, help_text='Is it compatible with GPL?')
+    copyleft =          models.BooleanField(blank=True, null=True, default=True, help_text='Is Copyleft a license?')
+    different_license = models.BooleanField(blank=True, null=True, help_text='Allows linking with code under a different license.')
+    active =            models.BooleanField(default=True, help_text='Check')
 
     class Meta:
         ordering = ['license_name']
--- MAIN/Licenses/views.py
+++ MAIN/Licenses/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse, JsonResponse
 from django.forms.models import model_to_dict
 from Licenses.models import Licenses
 from django.shortcuts import render
-from requirements_analysis import requirements_analyser
+import subprocess
+import json
+
 
 licenses_serializer = {
     'license_name': {'required': True, 'read only': False},
@@ -143,6 +145,21 @@ def get_update_delete_license(request, data_id):
         return delete_license(request, data_id)
     else:
         return HttpResponse('Error')
+
+
+def requirements_analyser(requirements):
+    requirements_file = open(requirements, 'r')
+    list_requirements = []
+    # for i in requirements_file:
+    #     list_requirements.append(i.rstrip())
+    [list_requirements.append(line.rstrip()) for line in requirements_file]
+    requirements_file.close()
+    end_requirements_file = ' '.join(list_requirements)
+    subprocess.call(f"pip-licenses --format=json --summary --packages {end_requirements_file} >licenses_list.json", shell=True)
+    json_file = open('licenses_list.json', 'r')
+    json_list = json.load(json_file)
+    json_file.close()
+    return json_list
 
 
 def license_comparison(request):
